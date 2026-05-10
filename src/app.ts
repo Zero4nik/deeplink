1e2f572f8f0590b2d3005173b9f4b4c4728c8053
Zero4nik <freefaer0tt@gmail.com>
2026-05-10 19:44:38 +0500

    fix: add Vercel origin to CORS

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app: Express = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://deeplink-ivory-rho.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
